4ffc7eb9fa57a55b0dde84a2af9e8e4051cfa2a4
Matthias Kaeppler <m.kaeppler@gmail.com>
2020-07-29 17:15:38 +0200

    Start support for graphicsmagick
    Not working yet

--- cmd/gitlab-resize-image/main.go
+++ cmd/gitlab-resize-image/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"strconv"
 	"os"
+	"os/exec"
 	"log"
+	"fmt"
+	"bytes"
+	// "io"
 
 	"gitlab.com/gitlab-org/gitlab-workhorse/internal/imageresizer"
 )
 
 func main() {
+	resizeStrategy := "gmagick"
 	imageURL := os.Getenv("WH_RESIZE_IMAGE_URL")
 	requestedWidth, err := strconv.Atoi(os.Getenv("WH_RESIZE_IMAGE_WIDTH"))
 
@@ -21,7 +26,12 @@ func main() {
 		log.Fatalln("Failed downloading image data:", err)
 	}
 
-	resizedImageData, _, err := imageresizer.ResizeImage(imageData, uint(requestedWidth), "")
+	var resizedImageData []byte
+	if resizeStrategy == "bimg"{
+		resizedImageData, _, err = imageresizer.ResizeImage(imageData, uint(requestedWidth), "")
+	} else {
+		resizedImageData, err = resizeImageGMagick(imageData, requestedWidth)
+	}
 
 	if err != nil {
 		log.Fatalln("Failed resizing image:", err)
@@ -35,4 +45,22 @@ func main() {
 	if bytesWritten != len(resizedImageData) {
 		log.Fatalf("Failed writing all image data (written bytes: %d, image bytes: %d)", bytesWritten, len(resizedImageData))
 	}
+}
+
+func resizeImageGMagick(imageData []byte, width int) ([]byte, error) {
+	// fmt.Sprintf("-resize %dx", width)
+	cmd := exec.Command("gm", "convert", "-resize", "100x", "-", "-")
+	var inBuffer, outBuffer bytes.Buffer
+	cmd.Stdin = &inBuffer
+	cmd.Stdout = &outBuffer
+	cmd.Stderr = os.Stderr
+
+	inBuffer.Write(imageData)
+
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+
+	return outBuffer.Bytes(), nil
 }
