d84e775aaca2903326fbd587836de1ebcedc7f1a
Aleksei Lipniagov <alipniagov@gitlab.com>
2020-08-13 16:36:05 +0300

    Use GraphicsMagick ftp and 1.3.34 version

--- _support/install_graphicsmagick.sh
+++ _support/install_graphicsmagick.sh
@@ -9,12 +9,13 @@ export LDFLAGS="-L$mydir/lib"
 cd $mydir
 
 # download sources
-curl -L http://ftp.icm.edu.pl/pub/unix/graphics/GraphicsMagick/1.3/GraphicsMagick-1.3.31.tar.gz | tar xvz
+curl -L ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/1.3/GraphicsMagick-1.3.34.tar.gz | tar xvz
+
 # zlib needed for png
-curl -L http://ftp.icm.edu.pl/pub/unix/graphics/GraphicsMagick/delegates/zlib-1.2.11.tar.gz | tar xvz
-curl -L http://ftp.icm.edu.pl/pub/unix/graphics/GraphicsMagick/delegates/libpng-1.6.28.tar.gz | tar xvz
-curl -L http://ftp.icm.edu.pl/pub/unix/graphics/GraphicsMagick/delegates/libwebp-1.0.0.tar.gz | tar xvz
-curl -L http://ftp.icm.edu.pl/pub/unix/graphics/GraphicsMagick/delegates/jpegsrc.v6b2.tar.gz | tar xvz
+curl -L ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/delegates/zlib-1.2.11.tar.gz | tar xvz
+curl -L ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/delegates/libpng-1.6.37.tar.gz | tar xvz
+curl -L ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/delegates/libwebp-1.0.0.tar.gz | tar xvz
+curl -L ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/delegates/jpegsrc.v6b2.tar.gz | tar xvz
 
 # use `less zlib-<TAB>/configure` to discover configure options
 (cd zlib-*           && ./configure --static                             --prefix=$mydir && make install)
