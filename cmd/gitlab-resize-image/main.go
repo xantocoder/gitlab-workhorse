166cd56fba18ef2beba356b842b56d8638d45fb6
Matthias Kaeppler <m.kaeppler@gmail.com>
2020-08-28 11:24:55 +0200

    Use BPF filters to allow required syscalls
    Still not working; blocks on a futex forever

--- cmd/gitlab-resize-image/main.go
+++ cmd/gitlab-resize-image/main.go
@@ -4,22 +4,70 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-
-	"golang.org/x/sys/unix"
+	"syscall"
 
 	"github.com/h2non/bimg"
+
+	seccomp "github.com/seccomp/libseccomp-golang"
 )
 
-// https://github.com/torvalds/linux/blob/master/include/uapi/linux/seccomp.h#L11
-const seccompModeStrict = 1
+var allowedSyscalls = []string{
+	"futex",
+	"mmap",
+	"openat",
+	"read",
+	"seccomp",
+	"fstat",
+	"close",
+	"mprotect",
+	"getdents64",
+	"pread64",
+	"brk",
+	"write",
+	"prctl",
+	"arch_prctl",
+	"stat",
+	"munmap",
+	"rt_sigaction",
+	"rt_sigprocmask",
+	"rt_sigreturn",
+	"access",
+	"getpid",
+	"clone",
+	"uname",
+	"fcntl",
+	"ftruncate",
+	"unlink",
+	"umask",
+	"sigaltstack",
+	"statfs",
+	"mlock",
+	"gettid",
+	"sched_getaffinity",
+	"set_tid_address",
+	"readlinkat",
+	"set_robust_list",
+	"prlimit64",
+	"getrandom",
+}
 
-func strictMode() {
-	log("Setting strict mode")
-	err := unix.Prctl(unix.PR_SET_SECCOMP, seccompModeStrict, 0, 0, 0)
+func enterSeccompMode() {
+	log("Entering seccomp mode")
+	// create a "reject all" filter that always returns "Operation not permitted"
+	filter, err := seccomp.NewFilter(seccomp.ActErrno.SetReturnCode(int16(syscall.EPERM)))
 	if err != nil {
-		log(err)
+		fail(err)
 	}
-	log("Strict mode set")
+	// allow only syscalls in the given list
+	for _, syscall := range allowedSyscalls {
+		id, err := seccomp.GetSyscallFromName(syscall)
+		if err != nil {
+			fail(err)
+		}
+		filter.AddRule(id, seccomp.ActAllow)
+	}
+	filter.Load()
+	log("Seccomp mode set")
 }
 
 func main() {
@@ -28,8 +76,7 @@ func main() {
 		fail("Failed reading source image:", err)
 	}
 
-	// NOT WORKING
-	// strictMode()
+	enterSeccompMode()
 
 	log("Resizing image...")
 	resizedImage, err := bimg.NewImage(imageData).Resize(200, 200)
