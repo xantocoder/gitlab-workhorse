8514eca8776569a96786425f0d13e09e4def1fd7
Alessio Caiazza <acaiazza@gitlab.com>
2020-08-28 15:04:19 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- _support/check_changelog.sh
+++ _support/check_changelog.sh
@@ -3,7 +3,7 @@
 set -e
 
 # we skip the changelog check if the merge requet title ends with "NO CHANGELOG"
-if echo "$CI_MERGE_REQUEST_TITLE" | grep 'NO CHANGELOG$' >/dev/null; then
+if echo "$CI_MERGE_REQUEST_TITLE" | grep -q ' NO CHANGELOG$'; then
     echo "Changelog not needed"
 
     exit 0
@@ -11,7 +11,7 @@ fi
 
 target=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-master}
 
-if git diff --name-only "origin/$target" -- changelogs/ | wc -l | grep -E -v "\b0\b" > /dev/null; then
+if git diff --name-only "origin/$target" | grep -q '^changelogs/' ; then
     echo "Changelog included"
 else
     echo "Please add a changelog running '_support/changelog'"
