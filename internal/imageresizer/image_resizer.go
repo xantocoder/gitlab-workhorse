8591372bdc1b27ee3b223f440d85422a11896fa2
Matthias Kaeppler <m.kaeppler@gmail.com>
2020-09-02 16:45:26 +0200

    Set OMP_NUM_THREADS=1
    Setting it to 0 was breaking libgomp and it did not work on every platform. Setting it to 1 seems to both work and actually make sense.

--- internal/imageresizer/image_resizer.go
+++ internal/imageresizer/image_resizer.go
@@ -173,7 +173,7 @@ func startResizeImageCommand(ctx context.Context, imageReader io.Reader, errorWr
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	cmd.Env = []string{
 		"GL_RESIZE_IMAGE_WIDTH=" + strconv.Itoa(int(width)),
-		"OMP_NUM_THREADS=0", // disable threading since that causes issues with seccomp
+		"OMP_NUM_THREADS=1", // disable multi-threading since it causes issues on some platforms
 	}
 
 	stdout, err := cmd.StdoutPipe()
