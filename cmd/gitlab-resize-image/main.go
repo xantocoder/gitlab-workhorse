a866e6bf01d32d2025634a68a36fe2be3a2fa4b3
nmilojevic1 <nmilojevic@gitlab.com>
2020-09-03 12:13:29 +0200

    Add go imaging library for image scaling

--- cmd/gitlab-resize-image/main.go
+++ cmd/gitlab-resize-image/main.go
@@ -1,88 +1,39 @@
 package main
 
-/*
-#cgo pkg-config: GraphicsMagickWand
-
-#include <stdio.h>
-#include <wand/magick_wand.h>
-
-void printMagickError(MagickWand *wand) {
-	char *err;
-	ExceptionType errType;
-
-	err = MagickGetException(wand, &errType);
-	fprintf(stderr, "MagickError %d - %s\n", errType, err);
-
-	MagickRelinquishMemory(err);
-}
-*/
 import "C"
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
+	"io"
 	"strconv"
+	"image"
+	"github.com/disintegration/imaging"
 )
 
 func main() {
-	widthParam := os.Getenv("GL_RESIZE_IMAGE_WIDTH")
-	requestedWidth, err := strconv.Atoi(widthParam)
-	if err != nil {
-		fail("Failed parsing GL_RESIZE_IMAGE_WIDTH; not a valid integer:", widthParam)
-	}
+ 	widthParam := os.Getenv("GL_RESIZE_IMAGE_WIDTH")
+ 	requestedWidth, err := strconv.Atoi(widthParam)
+ 	if err != nil {
+ 		fail("Failed parsing GL_RESIZE_IMAGE_WIDTH; not a valid integer:", widthParam)
+ 	}
 
-	args := os.Args
-	_, err = C.InitializeMagick(C.CString(args[0]))
-	if err != nil {
-		fail("Failed initializing GraphicsMagick:", err)
-	}
+ 	withSeccomp(func() {
+        var reader io.Reader = os.Stdin
+        src, extension, err := image.Decode(reader)
 
-	wand, err := C.NewMagickWand()
-	if err != nil {
-		fail("Failed obtaining MagickWand:", err)
-	}
-	defer C.DestroyMagickWand(wand)
+        if err != nil {
+            fail("failed to open image: %v", err)
+        }
 
-	imageData, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fail("Failed reading image data from stdin", err)
-	}
+   	    format, err := imaging.FormatFromExtension(extension)
 
-	withSeccomp(func() {
-		runMagick("read_image", wand, readImage(imageData))
-		runMagick("scale_image", wand, scaleImage(requestedWidth))
-		runMagick("write_image", wand, writeImage)
-	})
-}
+ 	    if err != nil {
+             fail("failed to find extension: %v", err)
+        }
 
-func runMagick(opName string, wand *C.MagickWand, op func(*C.MagickWand) C.uint) {
-	if op(wand) == C.MagickFail {
-		C.printMagickError(wand)
-		fail(opName, "failed")
-	}
-}
-
-func readImage(imageData []byte) func(*C.MagickWand) C.uint {
-	return func(wand *C.MagickWand) C.uint {
-		return C.MagickReadImageBlob(wand, (*C.uchar)(&imageData[0]), C.ulong(len(imageData)))
-	}
-}
-
-func writeImage(wand *C.MagickWand) C.uint {
-	defer C.fflush(C.stdout)
-	return C.MagickWriteImageFile(wand, C.stdout)
-}
-
-func scaleImage(requestedWidth int) func(*C.MagickWand) C.uint {
-	return func(wand *C.MagickWand) C.uint {
-		currentWidth := C.MagickGetImageWidth(wand)
-		currentHeight := C.MagickGetImageHeight(wand)
-		aspect := C.float(currentHeight) / C.float(currentWidth)
-		newWidth := C.float(requestedWidth)
-		newHeight := aspect * newWidth
-
-		return C.MagickScaleImage(wand, C.ulong(newWidth), C.ulong(newHeight))
-	}
+	    image := imaging.Resize(src, requestedWidth, 0, imaging.Lanczos)
+	    imaging.Encode(os.Stdout, image, format)
+ 	})
 }
 
 func fail(args ...interface{}) {
